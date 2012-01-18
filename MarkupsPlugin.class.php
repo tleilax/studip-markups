322ccbe638bbedc8fe67b11b3d0ae2fe1c08e5ff
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-01-18 21:18:56 +0100

    initial code

---
+++ MarkupsPlugin.class.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * MarkupsPlugin.class.php
+ *
+ * Integrates the following markups:
+ *
+ * - Markdown
+ * - Textile
+ *
+ * @author  Jan-Hendrik Willms <tleilax+studip@gmail.com>
+ * @version 1.0
+ */
+
+class MarkupsPlugin extends StudIPPlugin implements SystemPlugin {
+
+    public function __construct() {
+        parent::__construct();
+        StudipFormat::addStudipMarkup('textile', '\[markdown\]', '\[\/markdown\]', 'MarkupsPlugin::Markdown');
+        StudipFormat::addStudipMarkup('textile', '\[textile\]', '\[\/textile\]', 'MarkupsPlugin::Textile');
+    }
+    
+    public static function Markdown($markup, $matches, $contents) {
+        require 'vendor/markdown/markdown.php';
+        
+        return Markdown($contents);
+    }
+
+    public static function Textile($markup, $matches, $contents) {
+        require 'vendor/classTextile.php';
+        
+        $textile = new Textile;
+        return $textile->TextileThis($contents);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# MarkupsPlugin for Stud.IP
+
+Use *[markdown]...[/markdown]* respectively *[textile]...[/textile]* where
+formatting is applied.
+
+## Credits
+
+Uses the following implementations:
+
+- [Textile](http://textile.thresholdstate.com/) by Dean Allen
+- [PHP Markdown](http://michelf.com/projects/php-markdown/) by Michel Fortin
