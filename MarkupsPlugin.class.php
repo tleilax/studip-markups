87491ba9a1a3d4e0f84d132669698972c703a6a4
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-11-02 09:38:20 +0100

    fix naming *facepalm*

--- MarkupsPlugin.class.php
+++ MarkupsPlugin.class.php
@@ -8,14 +8,14 @@
  * - Textile
  *
  * @author  Jan-Hendrik Willms <tleilax+studip@gmail.com>
- * @version 1.0
+ * @version 1.0.1
  */
 
 class MarkupsPlugin extends StudIPPlugin implements SystemPlugin {
 
     public function __construct() {
         parent::__construct();
-        StudipFormat::addStudipMarkup('textile', '\[markdown\]', '\[\/markdown\]', 'MarkupsPlugin::Markdown');
+        StudipFormat::addStudipMarkup('markdown', '\[markdown\]', '\[\/markdown\]', 'MarkupsPlugin::Markdown');
         StudipFormat::addStudipMarkup('textile', '\[textile\]', '\[\/textile\]', 'MarkupsPlugin::Textile');
     }
     
